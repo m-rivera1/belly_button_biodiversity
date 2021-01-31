c75a7ee9ecf106b8b4336d4702871a483e3cd1bd
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-31 17:14:52 -0600

    first attempt at bar

--- static/plots/plots.js
+++ static/plots/plots.js
@@ -3,13 +3,34 @@ d3.json('data/samples.json').then(function(data_json) {
 
     // Grab values from the data json object to build the plots
     var otuIds = data_json.samples[0].otu_ids;
-    console.log(otuIds);
+   // console.log(otuIds);
+
+    var otuLabels = data_json.samples[0].otu_labels;
 
     var topTen = otuIds.slice(0,10);
-    console.log(topTen);
+    var topTenLabels = otuLabels.slice(0,10);
+    console.log(topTen, topTenLabels);
 
+    var trace = {
+        x: topTen,
+        y: topTenLabels,
+        text: data_json.samples[0].otu_labels,
+        type: "bar",
+        orientation: "h"
+    };
     
-    
+    var plotData = [trace];
+
+    var layout = {
+        title: "Belly button fungus Top Ten",
+        margin: {
+          l: 100,
+          r: 100,
+          t: 100,
+          b: 100
+        }
+      
+
     
     // var stock = data.dataset.dataset_code;
     // var startDate = data.dataset.start_date;
@@ -17,7 +38,6 @@ d3.json('data/samples.json').then(function(data_json) {
     // var dates = unpack(data.dataset.data, 0);
     // var closingPrices = unpack(data.dataset.data, 4);
 
-})
-
-
-
+};
+});
+Plotly.newPlot("plot", plotData, layout);
