83c9e1f5c5d763815d21d873efb27b5699052814
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-31 21:30:07 -0600

    still working on bar graph

--- static/plots/plots.js
+++ static/plots/plots.js
@@ -1,28 +1,32 @@
 
 
 d3.json('data/samples.json').then(function(data_json) {
-    console.log(data_json);
+console.log(data_json);
+
 
 // Grab values from the data json object to build the plots
 var otuIds = data_json.samples[0].otu_ids;
 console.log(otuIds);
 
-var otuLabels = data_json.samples[0].otu_labels;
+var otuLabels = data_json.samples[1].sample_values;
 console.log(otuLabels);
+
+var hover = data_json.samples[0].otu_labels;
+console.log(hover);
 
 
 var topTen = otuIds.slice(0,10)
 var topTenLabels = otuLabels.slice(0,10);
-console.log(topTen, topTenLabels);
+var hoverLabels = hover.slice(0,10);
+console.log(topTen, topTenLabels, hoverLabels);
 
 var trace = {
     x: topTen,
     y: topTenLabels,
-// text: data_json.samples[0].otu_labels,
+    text: hoverLabels,
     type: "bar",
     orientation: "h"
 };
-
 var plotData = [trace];    
 
 
@@ -34,9 +38,13 @@ var layout = {
         l: 100,
         r: 100,
         t: 100,
-        b: 100 
+        b: 100, 
    
     }
-};})
 
-Plotly.newPlot("plot", plotData, layout);
+ 
+};
+
+
+Plotly.newPlot("plot", plotData , layout);
+});
