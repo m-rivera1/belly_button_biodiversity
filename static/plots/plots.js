2f938012e5e10e2b9688d55c94cd71507d287d0f
m-rivera1 <mike.11.mxr@gmail.com>
2021-01-31 18:46:04 -0600

    more changes to bar graph

--- static/plots/plots.js
+++ static/plots/plots.js
@@ -1,43 +1,42 @@
+
+
 d3.json('data/samples.json').then(function(data_json) {
     console.log(data_json);
 
-    // Grab values from the data json object to build the plots
-    var otuIds = data_json.samples[0].otu_ids;
-   // console.log(otuIds);
+// Grab values from the data json object to build the plots
+var otuIds = data_json.samples[0].otu_ids;
+console.log(otuIds);
 
-    var otuLabels = data_json.samples[0].otu_labels;
+var otuLabels = data_json.samples[0].otu_labels;
+console.log(otuLabels);
 
-    var topTen = otuIds.slice(0,10);
-    var topTenLabels = otuLabels.slice(0,10);
-    console.log(topTen, topTenLabels);
 
-    var trace = {
-        x: topTen,
-        y: topTenLabels,
-        text: data_json.samples[0].otu_labels,
-        type: "bar",
-        orientation: "h"
-    };
-    
-    var plotData = [trace];
+var topTen = otuIds.slice(0,10)
+var topTenLabels = otuLabels.slice(0,10);
+console.log(topTen, topTenLabels);
 
-    var layout = {
-        title: "Belly button fungus Top Ten",
-        margin: {
-          l: 100,
-          r: 100,
-          t: 100,
-          b: 100
-        }
-      
-
-    
-    // var stock = data.dataset.dataset_code;
-    // var startDate = data.dataset.start_date;
-    // var endDate = data.dataset.end_date;
-    // var dates = unpack(data.dataset.data, 0);
-    // var closingPrices = unpack(data.dataset.data, 4);
-
+var trace = {
+    x: topTen,
+    y: topTenLabels,
+// text: data_json.samples[0].otu_labels,
+    type: "bar",
+    orientation: "h"
 };
-});
+
+var plotData = [trace];    
+
+
+var layout = {
+    title: "Belly button fungus Top Ten",
+    yaxis:{tickmode: "linear",
+    },
+    margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100 
+   
+    }
+};})
+
 Plotly.newPlot("plot", plotData, layout);
