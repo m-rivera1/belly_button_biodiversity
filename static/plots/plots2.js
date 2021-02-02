3ec101980b269cd08dddd9d17130068c88e1aeb0
m-rivera1 <mike.11.mxr@gmail.com>
2021-02-01 22:54:23 -0600

    finially finished bubble chart..was missing the create data var step

--- static/plots/plots2.js
+++ static/plots/plots2.js
@@ -1,15 +1,92 @@
-//function data_info(id){
-//    d3.json("samples.json").then((data)=> {
-//        var metadata = data.metadata;
-//        console.log(metadata)
-
-        
-  //  });
-//};
 
 
-var mydata = JSON.parse(data);
-alert(mydata[0].name);
-alert(mydata[0].age);
-alert(mydata[1].name);
-alert(mydata[1].age);
+d3.json('data/samples.json').then(function(data_json) {
+  console.log(data_json);
+  
+  
+  // Grab values from the data json object to build the plots
+  var otuIds = data_json.samples[0].otu_ids;
+  console.log(otuIds);
+  
+  var otuLabels = data_json.samples[1].sample_values.slice(0,10);
+  console.log(otuLabels);
+  
+  var hover = data_json.samples[1].otu_labels;
+  console.log(hover);
+  
+  
+  var topTen = otuIds.slice(0,10).reverse()
+  var topTen2 = topTen.map(label => "OTU " + label);
+  
+  var topTenLabels = otuLabels.reverse();
+  
+  
+  var hoverLabels = hover.slice(0,10);
+  
+  console.log(topTen, hoverLabels, topTenLabels);
+  
+  
+  var trace = {
+      x: topTenLabels,
+      y: topTen2 ,
+      text: hoverLabels,
+      type: "bar",
+      orientation: "h",
+      xaxis: {title: 'Count'},
+      
+  };
+  var barData = [trace];    
+  
+  
+  var layout = {
+      title: "Top Ten Operational Taxonomic Units (OTU's)",
+      yaxis:{tickmode: "linear",
+      },
+      margin: {
+          l: 100,
+          r: 100,
+          t: 100,
+          b: 100, 
+     
+      }
+  
+   
+  };
+  
+  
+  Plotly.newPlot("plot", barData , layout);
+  })
+
+  d3.json('data/samples.json').then(function(data_json) {
+      
+      xData = data_json.samples[0].otu_ids;
+      yData = data_json.samples[0].sample_values;
+      xLabels = data_json.samples[0].otu_ids;
+  
+      console.log(xData, yData, xLabels)
+  
+      
+  
+      var traceBubble = {
+        x: xData,
+        y: yData,
+        text: xLabels,
+        mode: 'markers',
+          marker: {
+          size: yData,
+          color: xData
+        }
+  };
+  
+              
+      var layoutBubble = {
+          xaxis:{title: "OTU ID"},
+          showlegend: false,
+          height: 600,
+          width: 600,
+ 
+  };  
+
+  Plotly.newPlot('bubble', traceBubble, layoutBubble);
+
+});
